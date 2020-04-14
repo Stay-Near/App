49611e57823422a77147104266d3c5d66375aad6
Kenshex7 <hector12x13@gmail.com>
2020-04-14 17:15:20 -0500

    Getters y setters para id y photo
    Signed-off-by: Kenshex7 <hector12x13@gmail.com>

--- app/src/main/java/com/example/staynear/model/Room.java
+++ app/src/main/java/com/example/staynear/model/Room.java
@@ -23,6 +23,22 @@ public class Room {
         this.photo = photo;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
