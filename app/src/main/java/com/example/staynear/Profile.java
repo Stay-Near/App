ead9c87eeb90f07d9884fb48d421c1262deda7f3
JuanMelendres <juan.mele97@gmail.com>
2020-06-08 10:26:26 -0500

    Add Change name and number
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- app/src/main/java/com/example/staynear/Profile.java
+++ app/src/main/java/com/example/staynear/Profile.java
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,9 +22,14 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends AppCompatActivity {
 
+    private String userName, mat, cor, tel;
     private TextView name, name2, matricula, matricula2, mail, telefono;
-    private DatabaseReference reff;
-    private FirebaseUser currentFirebaseUser;
+    private DatabaseReference reff, mReff;
+    private FirebaseAuth fAuth;
+    private FirebaseUser currentUser;
+    private RatingBar ratingBar;
+    private Button update_data;
+    String user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +38,23 @@ public class Profile extends AppCompatActivity {
 
         name = findViewById(R.id.name);
         matricula = findViewById(R.id.matricula);
-        name2 = findViewById(R.id.name2);
-        matricula2 = findViewById(R.id.matricula2);
-        mail = findViewById(R.id.correo);
-        telefono = findViewById(R.id.numero);
+        name2 = (EditText) findViewById(R.id.name2);
+        matricula2 = (EditText) findViewById(R.id.matricula2);
+        mail = (EditText) findViewById(R.id.correo);
+        telefono = (EditText) findViewById(R.id.numero);
 
-        reff = FirebaseDatabase.getInstance().getReference().child("user").child("53c65fcd-01a6-4085-a00b-d97c74de3501");
+        fAuth = FirebaseAuth.getInstance();
+        currentUser = fAuth.getCurrentUser();
+
+        user_id = currentUser.getUid();
+
+        reff = FirebaseDatabase.getInstance().getReference().child("user").child(user_id);
+        mReff = FirebaseDatabase.getInstance().getReference().child("user").child(user_id);
+
+        showData();
+    }
+
+    public void showData() {
         reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -42,11 +63,13 @@ public class Profile extends AppCompatActivity {
                 String num = dataSnapshot.child("telefono").getValue().toString();
 
                 name.setText(nombre);
-                matricula.setText("A00451398");
                 name2.setText(nombre);
-                matricula2.setText("A00451398");
                 mail.setText(correo);
                 telefono.setText(num);
+
+                userName = nombre;
+                cor = correo;
+                tel = num;
             }
 
             @Override
@@ -55,8 +78,57 @@ public class Profile extends AppCompatActivity {
         });
     }
 
-    public void changeToMainMenuActivity(View v){
+    public void updateData(View view) {
+        if(isNameChanged()) {
+            Toast.makeText(this, "Data has been updated", Toast.LENGTH_SHORT).show();
+            showData();
+        }
+        else if(isMailChanged()) {
+            Toast.makeText(this, "Data has been updated", Toast.LENGTH_SHORT).show();
+            showData();
+        }
+        else if(isPhoneChanged()) {
+            Toast.makeText(this, "Data has been updated", Toast.LENGTH_SHORT).show();
+            showData();
+        }
+        else {
+            Toast.makeText(this, "Data is the same and can not be updated", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isPhoneChanged() {
+        if(!tel.equals(telefono.getText().toString())) {
+            mReff.child("telefono").setValue(telefono.getText().toString());
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean isMailChanged() {
+        if(!cor.equals(mail.getText().toString())) {
+            mReff.child("correo").setValue(mail.getText().toString());
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean isNameChanged() {
+        if(!userName.equals(name2.getText().toString())) {
+            mReff.child("nombre").setValue(name2.getText().toString());
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void changeToMainMenuActivity(View v) {
         Intent change = new Intent(this, MainActivity.class);
         startActivity(change);
     }
+
 }
